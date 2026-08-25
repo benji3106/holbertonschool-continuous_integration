0f6954cec4ee7d92cde45aeda5ecf885d741621a
benji3106 <benjamin3106@live.fr>
2026-08-25 14:58:45 +0200

    test: introduce deliberately failing test

--- index.test.js
+++ index.test.js
@@ -3,5 +3,5 @@ import assert from "node:assert";
 import { add } from "./index.js";
 
 test("add() returns the sum of two numbers", () => {
-  assert.strictEqual(add(2, 3), 5);
+  assert.strictEqual(add(2, 3), 999);
 });
